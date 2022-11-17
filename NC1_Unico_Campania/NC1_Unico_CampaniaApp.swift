bc4f2952d1e51d77b9ba7af144331ce6630cd12d
MariaVittoriaAgosti <113709068+MariaVittoriaAgosti@users.noreply.github.com>
2022-11-17 16:14:03 +0100

    Initial Commit

---
+++ NC1_Unico_Campania/NC1_Unico_CampaniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_Unico_CampaniaApp.swift
+//  NC1_Unico_Campania
+//
+//  Created by Maria Vittoria Agosti on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NC1_Unico_CampaniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
